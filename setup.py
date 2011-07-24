8cdfe6b196e7bf0c26ffc0f5b2298937f3107a03
Lars Wirzenius <liw@liw.fi>
2011-07-24 11:11:14 +0100

    Update setup.py for PyPI registration.

--- setup.py
+++ setup.py
@@ -1,29 +1,43 @@
-# Copyright 2010  Lars Wirzenius
-# 
-# This program is free software: you can redistribute it and/or modify
+#!/usr/bin/python
+# Copyright (C) 2010, 2011  Lars Wirzenius <liw@liw.fi>
+#
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 from distutils.core import setup
 
 import ttystatus
 
-setup(
-    name='ttystatus',
-    version=ttystatus.__version__,
-    description='terminal progress bar and status output for command line',
-    author='Lars Wirzenius',
-    author_email='liw@liw.fi',
-#    url='http://liw.fi/ttystatus/',
-    packages=['ttystatus'],
-)
+setup(name='ttystatus',
+      version=ttystatus.__version__,
+      description='terminal progress bar and status output for command line',
+      long_description='''\
+The ttystatus Python library provides a simple widget-based way to show
+progress and current status in command line programs.
+''',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Terminals',
+      ],
+      author='Lars Wirzenius',
+      author_email='liw@liw.fi',
+      url='http://liw.fi/ttystatus/',
+      packages=['ttystatus'],
+     )
