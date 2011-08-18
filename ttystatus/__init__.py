ca6410bcda6063cd7a45dcb8262c1273255ec103
Lars Wirzenius <liw@liw.fi>
2011-08-18 19:06:19 +0100

    Fix version number.

--- ttystatus/__init__.py
+++ ttystatus/__init__.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = '0.12'
+__version__ = '0.13'
 
 from messager import Messager
 from status import TerminalStatus
