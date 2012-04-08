13562fd7948942bc52cb35d0ae1b51bb61fa69a0
Lars Wirzenius <liw@liw.fi>
2012-04-08 09:49:54 +0100

    Release version 0.16

--- ttystatus/__init__.py
+++ ttystatus/__init__.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = '0.15'
+__version__ = '0.16'
 
 from messager import Messager
 from status import TerminalStatus
