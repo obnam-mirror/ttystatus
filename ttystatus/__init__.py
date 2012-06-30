2689b34c8bb1376b11b23b89109d53488b65de74
Lars Wirzenius <liw@liw.fi>
2012-06-30 16:52:14 +0100

    Prepare to release version 0.19

--- ttystatus/__init__.py
+++ ttystatus/__init__.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = '0.18'
+__version__ = '0.19'
 
 from messager import Messager
 from status import TerminalStatus
