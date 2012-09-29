9787129acc4c3798c9cbbc25316f1469f9faf6a1
Lars Wirzenius <liw@liw.fi>
2012-09-29 09:30:28 +0100

    Change version number from 0.20.1 to 0.21 due to jenkinstool

--- ttystatus/__init__.py
+++ ttystatus/__init__.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = '0.20.1'
+__version__ = '0.21'
 
 from messager import Messager
 from status import TerminalStatus
